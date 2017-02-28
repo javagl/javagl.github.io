93e44c60e7fb0a8bcc80d25e496e5b31358877e6
Marco Hutter <javagl@javagl.de>
2017-02-28 14:38:34 +0100

    Generalized to combine RGB channels
    Somehow related to https://github.com/KhronosGroup/glTF/issues/857

--- MetallicRoughnessCreator/scripts/MetallicRoughnessCreator.js
+++ MetallicRoughnessCreator/scripts/MetallicRoughnessCreator.js
@@ -1,100 +1,134 @@
 
-var inputImageDatas = [undefined, undefined];
+var inputImageDatas = [undefined, undefined, undefined];
 var outputImageData;
 
-
-function combine(metallicImageData, roughnessImageData, outputImageData, flip)
+function isUndefined(object)
 {
-    var metallicPixelData = metallicImageData.data;
-    var roughnessPixelData = roughnessImageData.data;
+    return (typeof object == "undefined");
+}
+
+function isDefined(object)
+{
+    return !isUndefined(object);
+}
+
+function combine(outputImageData, flip)
+{
     var outputPixelData = outputImageData.data;
-    
     var w = outputImageData.width;
     var h = outputImageData.height;
-    for (var y = 0; y < h; y++)
+
+    for (var i = 0; i < 3; i++)
     {
-        var metallicOffset = y * metallicImageData.width;
-        var roughnessOffset = y * roughnessImageData.width;
-        var outputOffset = y * outputImageData.width;
-        if (flip)
+        var inputImageData = inputImageDatas[i];
+        if (isUndefined(inputImageData))
         {
-            outputOffset = (outputImageData.height - 1 - y) * outputImageData.width
+            continue;
         }
-        for (var x = 0; x < w; x++)
+        var inputPixelData = inputImageData.data;
+        for (var y = 0; y < h; y++)
         {
-            var metallicIndex = x + metallicOffset;
-            var red = metallicPixelData[metallicIndex << 2];
-            
-            var roughnessIndex = x + roughnessOffset;
-            var green = roughnessPixelData[roughnessIndex << 2];
-            
-            var outputIndex = x + outputOffset;
-            
-            outputPixelData[(outputIndex << 2) + 0] = red;
-            outputPixelData[(outputIndex << 2) + 1] = green;
-            outputPixelData[(outputIndex << 2) + 2] = 0;
-            outputPixelData[(outputIndex << 2) + 3] = 255;
+            var inputOffset = y * inputImageData.width;
+            var outputOffset = y * outputImageData.width;
+            if (flip)
+            {
+                outputOffset = (outputImageData.height - 1 - y) * outputImageData.width
+            }
+            for (var x = 0; x < w; x++)
+            {
+                var inputIndex = x + inputOffset;
+                var value = inputPixelData[inputIndex << 2];
+                var outputIndex = x + outputOffset;
+                outputPixelData[(outputIndex << 2) + i] = value;
+                outputPixelData[(outputIndex << 2) + 3] = 255;
+            }
         }
+    }
+}
+
+function haveEqualSize(imageDataA, imageDataB)
+{
+    if (isUndefined(imageDataA))
+    {
+        return true;
+    }
+    if (isUndefined(imageDataB))
+    {
+        return true;
+    }
+    return imageDataA.width == imageDataB.width && imageDataA.height == imageDataB.height;
+}
+
+
+function checkEqualWidths(nameA, imageDataA, nameB, imageDataB, infoElement)
+{
+    if (!haveEqualSize(imageDataA, imageDataB))
+    {
+        infoElement.innerHTML +=
+            "Warning: Different size: " +
+            nameA + " is " + imageDataA.width + "x" + imageDataA.height + " and " +
+            nameB + " is " + imageDataB.width + "x" + imageDataB.height + "<br>";
     }
 }
 
 
 function updateOutput()
 {
-    if (inputImageDatas[0] === undefined)
-    {
-        return;  
-    }
-    if (inputImageDatas[1] === undefined)
-    {
-        return;  
-    }
-    
     var infoElement = document.getElementById("outputImageInfo");
-    
-    var metallicImageData = inputImageDatas[0];
-    var roughnessImageData = inputImageDatas[1];
-    
-    if (metallicImageData.width != roughnessImageData.width)
+
+    infoElement.innerHTML = ""
+
+    var rImageData = inputImageDatas[0];
+    var gImageData = inputImageDatas[1];
+    var bImageData = inputImageDatas[2];
+
+    if (isUndefined(rImageData) &&
+        isUndefined(gImageData) &&
+        isUndefined(bImageData))
     {
-        infoElement.innerHTML += 
-          "Warning: Different widths: " + metallicImageData.width + 
-          " and "+ roughnessImageData.width + "<br>";
+        infoElement.innerHTML +=
+            "No input images" + "<br>";
+        return;
     }
-    if (metallicImageData.height != roughnessImageData.height)
-    {
-        infoElement.innerHTML += 
-          "Warning: Different heights: " + metallicImageData.height + 
-          " and "+ roughnessImageData.height + "<br>";
-    }
-    
-    var width = Math.min(metallicImageData.width, roughnessImageData.width);
-    var height = Math.min(metallicImageData.height, roughnessImageData.height);
-    
+
+    checkEqualWidths("r", rImageData, "g", gImageData, infoElement);
+    checkEqualWidths("g", gImageData, "b", bImageData, infoElement);
+    checkEqualWidths("r", rImageData, "b", bImageData, infoElement);
+
+
+    var width = (1<<16);
+    if (isDefined(rImageData)) width = Math.min(width, rImageData.width);
+    if (isDefined(gImageData)) width = Math.min(width, gImageData.width);
+    if (isDefined(bImageData)) width = Math.min(width, bImageData.width);
+
+    var height = (1<<16);
+    if (isDefined(rImageData)) height = Math.min(height, rImageData.height);
+    if (isDefined(gImageData)) height = Math.min(height, gImageData.height);
+    if (isDefined(bImageData)) height = Math.min(height, bImageData.height);
+
     var canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
     var context = canvas.getContext('2d');
     outputImageData = context.createImageData(width, height);
-    
+
     var flip = document.getElementById("flipVerticallyCheckBox").checked;
-    combine(metallicImageData, roughnessImageData, outputImageData, flip);
-    
+    combine(outputImageData, flip);
+
     context.putImageData(outputImageData, 0, 0);
 
-
     var typeForm = document.getElementById("typeForm");
-    if (typeForm.elements[0].checked) 
+    if (typeForm.elements[0].checked)
     {
         var outputImage = document.getElementById("outputImage");
         outputImage.src = canvas.toDataURL("image/png");
     }
-    if (typeForm.elements[1].checked) 
+    if (typeForm.elements[1].checked)
     {
         var outputImage = document.getElementById("outputImage");
         outputImage.src = canvas.toDataURL("image/jpeg", 0.9);
     }
-    if (typeForm.elements[2].checked) 
+    if (typeForm.elements[2].checked)
     {
         var outputImage = document.getElementById("outputImage");
         outputImage.src = canvas.toDataURL("image/jpeg", 0.7);
@@ -113,12 +147,12 @@ function imageFileLoaded(event, index, image, infoElement)
         //updateOutput();
     };
     fullImage.src = event.target.result;
-    
+
     image.src = event.target.result;
 }
 
 
-function obtainImageData(image) 
+function obtainImageData(image)
 {
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
@@ -131,7 +165,7 @@ function obtainImageData(image)
 };
 
 
-function handleFileSelect(event, index, image, infoElement) 
+function handleFileSelect(event, index, image, infoElement)
 {
     event.stopPropagation();
     event.preventDefault();
@@ -142,24 +176,24 @@ function handleFileSelect(event, index, image, infoElement)
         infoElement.innerHTML = "Only a single file is allowed. Dropped "+fileList.length+" files";
         return;
     }
-    
+
     var file = fileList[0];
-    if (!file.type.match('image.*')) 
+    if (!file.type.match('image.*'))
     {
         infoElement.innerHTML = "Expected image file, but found "+file.type;
         return;
     }
 
     var reader = new FileReader();
-    reader.onload = function(e) 
+    reader.onload = function(e)
     {
         imageFileLoaded(e, index, image, infoElement);
     };
     reader.readAsDataURL(file);
-    
+
 }
 
-function handleDragOver(event) 
+function handleDragOver(event)
 {
     event.stopPropagation();
     event.preventDefault();
